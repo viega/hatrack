a0745ebc73a01ebc80470d7dc287d64749df9413
John Viega <john@zork.org>
2022-01-16 00:08:56 -0500

    Added draft methods for keys/values/items

--- src/hatrack_dict.c
+++ src/hatrack_dict.c
@@ -39,12 +39,12 @@ hatrack_dict_init(hatrack_dict_t *self, uint32_t key_type)
     case HATRACK_DICT_KEY_TYPE_OBJ_INT:
     case HATRACK_DICT_KEY_TYPE_OBJ_REAL:
     case HATRACK_DICT_KEY_TYPE_OBJ_CSTR:
-    case HATRACK_DICT_KEY_TYPE_OBJ_PTR:
+    case HATRACK_DICT_KEY_TYPE_OBJ_PTR:			
     case HATRACK_DICT_KEY_TYPE_OBJ_CUSTOM:
-        self->key_type = key_type;
-        break;
+	self->key_type = key_type;
+	break;
     default:
-        abort();
+	abort();
     }
 
     self->hash_info.offsets.hash_offset  = 0;
@@ -61,27 +61,26 @@ hatrack_dict_cleanup(hatrack_dict_t *self)
     witchhat_bucket_t *bucket;
     hatrack_hash_t     hv;
     witchhat_record_t  record;
-
+    
     if (self->free_handler) {
-        for (i = 0; i <= self->witchhat_instance.store_current->last_slot;
-             i++) {
-            bucket = &self->witchhat_instance.store_current->buckets[i];
-            hv     = atomic_load(&bucket->hv);
+	for (i = 0; i <= self->witchhat_instance.store_current->last_slot; i++) {
+	    bucket = &self->witchhat_instance.store_current->buckets[i];
+	    hv     = atomic_load(&bucket->hv);
+	    
+	    if (hatrack_bucket_unreserved(hv)) {
+		continue;
+	    }
 
-            if (hatrack_bucket_unreserved(hv)) {
-                continue;
-            }
+	    record = atomic_load(&bucket->record);
 
-            record = atomic_load(&bucket->record);
+	    if (!record.info) {
+		continue;
+	    }
 
-            if (!record.info) {
-                continue;
-            }
-
-            (*self->free_handler)((hatrack_dict_item_t *)record.item);
-        }
+	    (*self->free_handler)((hatrack_dict_item_t *)record.item);
+	}
     }
-
+    
     mmm_retire(atomic_load(&self->witchhat_instance.store_current));
 
     return;
@@ -129,15 +128,15 @@ hatrack_dict_get(hatrack_dict_t *self, void *key, bool *found)
     item = witchhat_get(&self->witchhat_instance, hv, found);
 
     if (!item) {
-        if (found) {
-            *found = false;
-        }
-
-        return NULL;
+	if (found) {
+	    *found = false;
+	}
+	
+	return NULL;
     }
 
     if (found) {
-        *found = true;
+	*found = true;
     }
 
     return item->value;
@@ -149,26 +148,26 @@ hatrack_dict_put(hatrack_dict_t *self, void *key, void *value)
     hatrack_hash_t       hv;
     hatrack_dict_item_t *new_item;
     hatrack_dict_item_t *old_item;
-
+    
     hv = hatrack_dict_get_hash_value(self, key);
 
     mmm_start_basic_op();
-
+    
     new_item        = mmm_alloc_committed(sizeof(hatrack_dict_item_t));
     new_item->key   = key;
     new_item->value = value;
-
+    
     old_item = witchhat_put(&self->witchhat_instance, hv, new_item, NULL);
 
     if (old_item) {
-        if (self->free_handler) {
-            old_item->associated_dict = self;
-            mmm_add_cleanup_handler(old_item, hatrack_dict_record_cleanup);
-        }
-
-        mmm_retire(old_item);
+	if (self->free_handler) {
+	    old_item->associated_dict = self;	    
+	    mmm_add_cleanup_handler(old_item, hatrack_dict_record_cleanup);
+	}
+	
+	mmm_retire(old_item);
     }
-
+			    
     mmm_end_op();
 
     return;
@@ -192,15 +191,15 @@ hatrack_dict_replace(hatrack_dict_t *self, void *key, void *value)
     old_item = witchhat_replace(&self->witchhat_instance, hv, new_item, NULL);
 
     if (old_item) {
-        if (self->free_handler) {
-            old_item->associated_dict = self;
-            mmm_add_cleanup_handler(old_item, hatrack_dict_record_cleanup);
-        }
+	if (self->free_handler) {
+	    old_item->associated_dict = self;	    
+	    mmm_add_cleanup_handler(old_item, hatrack_dict_record_cleanup);
+	}
+	
+	mmm_retire(old_item);
+	mmm_end_op();
 
-        mmm_retire(old_item);
-        mmm_end_op();
-
-        return true;
+	return true;
     }
 
     mmm_retire_unused(new_item);
@@ -222,11 +221,11 @@ hatrack_dict_add(hatrack_dict_t *self, void *key, void *value)
     new_item        = mmm_alloc_committed(sizeof(hatrack_dict_item_t));
     new_item->key   = key;
     new_item->value = value;
-
+    
     if (witchhat_add(&self->witchhat_instance, hv, new_item)) {
-        mmm_end_op();
-
-        return true;
+	mmm_end_op();
+	
+	return true;
     }
 
     mmm_retire_unused(new_item);
@@ -248,77 +247,218 @@ hatrack_dict_remove(hatrack_dict_t *self, void *key)
     old_item = witchhat_remove(&self->witchhat_instance, hv, NULL);
 
     if (old_item) {
-        if (self->free_handler) {
-            old_item->associated_dict = self;
-            mmm_add_cleanup_handler(old_item, hatrack_dict_record_cleanup);
-        }
+	if (self->free_handler) {
+	    old_item->associated_dict = self;
+	    mmm_add_cleanup_handler(old_item, hatrack_dict_record_cleanup);
+	}
+	
+	mmm_retire(old_item);
+	mmm_end_op();
 
-        mmm_retire(old_item);
-        mmm_end_op();
-
-        return true;
+	return true;
     }
 
     mmm_end_op();
-
+    
     return false;
 }
+
+hatrack_dict_key_t *
+hatrack_dict_keys(hatrack_dict_t *self, uint64_t *num)
+{
+    hatrack_view_t      *view;
+    hatrack_dict_key_t  *ret;
+    hatrack_dict_item_t *item;
+    uint64_t             alloc_len;
+    uint32_t             i;
+
+    view      = witchhat_view(&self->witchhat_instance, num, false);
+    alloc_len = sizeof(hatrack_dict_key_t) * *num;
+    ret       = (hatrack_dict_key_t *)malloc(alloc_len);
+
+    for (i = 0; i < *num; i++) {
+	item   = (hatrack_dict_item_t *)view[i].item;
+	ret[i] = item->key;
+    }
+
+    free(view);
+
+    return ret;
+}
+
+hatrack_dict_value_t *
+hatrack_dict_values(hatrack_dict_t *self, uint64_t *num)
+{
+    hatrack_view_t       *view;
+    hatrack_dict_value_t *ret;
+    hatrack_dict_item_t  *item;
+    uint64_t              alloc_len;
+    uint32_t              i;
+
+    view      = witchhat_view(&self->witchhat_instance, num, false);
+    alloc_len = sizeof(hatrack_dict_key_t) * *num;
+    ret       = (hatrack_dict_key_t *)malloc(alloc_len);
+
+    for (i = 0; i < *num; i++) {
+	item   = (hatrack_dict_item_t *)view[i].item;
+	ret[i] = item->value;
+    }
+
+    free(view);
+    
+    return ret;
+}
+
+hatrack_dict_item_t *
+hatrack_dict_items(hatrack_dict_t *self, uint64_t *num)
+{
+    hatrack_view_t      *view;
+    hatrack_dict_item_t *ret;
+    hatrack_dict_item_t *item;
+    uint64_t             alloc_len;
+    uint32_t             i;
+
+    view      = witchhat_view(&self->witchhat_instance, num, false);
+    alloc_len = sizeof(hatrack_dict_key_t) * *num;
+    ret       = (hatrack_dict_item_t *)calloc(1, alloc_len);
+
+    for (i = 0; i < *num; i++) {
+	item         = (hatrack_dict_item_t *)view[i].item;
+	ret[i].key   = item->key;
+	ret[i].value = item->value;
+    }
+
+    free(view);
+    
+    return ret;
+}
+
+hatrack_dict_key_t *
+hatrack_dict_keys_sort(hatrack_dict_t *self, uint64_t *num)
+{
+    hatrack_view_t      *view;
+    hatrack_dict_key_t  *ret;
+    hatrack_dict_item_t *item;
+    uint64_t             alloc_len;
+    uint32_t             i;
+
+    view      = witchhat_view(&self->witchhat_instance, num, true);
+    alloc_len = sizeof(hatrack_dict_key_t) * *num;
+    ret       = (hatrack_dict_key_t *)malloc(alloc_len);
+
+    for (i = 0; i < *num; i++) {
+	item   = (hatrack_dict_item_t *)view[i].item;
+	ret[i] = item->key;
+    }
+
+    free(view);
+
+    return ret;
+}
+
+hatrack_dict_value_t *
+hatrack_dict_values_sort(hatrack_dict_t *self, uint64_t *num)
+{
+    hatrack_view_t       *view;
+    hatrack_dict_value_t *ret;
+    hatrack_dict_item_t  *item;
+    uint64_t              alloc_len;
+    uint32_t              i;
+
+    view      = witchhat_view(&self->witchhat_instance, num, true);
+    alloc_len = sizeof(hatrack_dict_key_t) * *num;
+    ret       = (hatrack_dict_key_t *)malloc(alloc_len);
+
+    for (i = 0; i < *num; i++) {
+	item   = (hatrack_dict_item_t *)view[i].item;
+	ret[i] = item->value;
+    }
+
+    free(view);
+    
+    return ret;
+}
+
+hatrack_dict_item_t *
+hatrack_dict_items_sort(hatrack_dict_t *self, uint64_t *num)
+{
+    hatrack_view_t      *view;
+    hatrack_dict_item_t *ret;
+    hatrack_dict_item_t *item;
+    uint64_t             alloc_len;
+    uint32_t             i;
+
+    view      = witchhat_view(&self->witchhat_instance, num, true);
+    alloc_len = sizeof(hatrack_dict_key_t) * *num;
+    ret       = (hatrack_dict_item_t *)calloc(1, alloc_len);
+
+    for (i = 0; i < *num; i++) {
+	item         = (hatrack_dict_item_t *)view[i].item;
+	ret[i].key   = item->key;
+	ret[i].value = item->value;
+    }
+
+    free(view);
+    
+    return ret;
+}
+
 
 static hatrack_hash_t
 hatrack_dict_get_hash_value(hatrack_dict_t *self, void *key)
 {
     hatrack_hash_t hv;
     int32_t        offset;
-
+    
     switch (self->key_type) {
     case HATRACK_DICT_KEY_TYPE_OBJ_CUSTOM:
-        return (*self->hash_info.custom_hash)(key);
-
+	return (*self->hash_info.custom_hash)(key);
+	
     case HATRACK_DICT_KEY_TYPE_INT:
-        return hash_int((uint64_t)key);
-
+	return hash_int((uint64_t)key);
+	
     case HATRACK_DICT_KEY_TYPE_REAL:
-        return hash_double(*(double *)key);
-
+	return hash_double(*(double *)key);
+	
     case HATRACK_DICT_KEY_TYPE_CSTR:
-        return hash_cstr((char *)key);
+	return hash_cstr((char *)key);
 
     case HATRACK_DICT_KEY_TYPE_PTR:
-        return hash_pointer(key);
+	return hash_pointer(key);
 
     default:
-        break;
+	break;
     }
 
     offset = self->hash_info.offsets.cache_offset;
-
+    
     if (offset != (int32_t)HATRACK_DICT_NO_CACHE) {
-        hv = *(hatrack_hash_t *)(((uint8_t *)key) + offset);
-
-        if (!hatrack_bucket_unreserved(hv)) {
-            return hv;
-        }
+	hv = *(hatrack_hash_t *)(((uint8_t *)key) + offset);
+	
+	if (!hatrack_bucket_unreserved(hv)) {
+	    return hv;
+	}
     }
 
     switch (self->key_type) {
     case HATRACK_DICT_KEY_TYPE_OBJ_INT:
-        hv = hash_int((uint64_t)key);
-        break;
+	hv = hash_int((uint64_t)key);
+	break;
     case HATRACK_DICT_KEY_TYPE_OBJ_REAL:
-        hv = hash_double(*(double *)key);
-        break;
+	hv = hash_double(*(double *)key);
+	break;
     case HATRACK_DICT_KEY_TYPE_OBJ_CSTR:
-        hv = hash_cstr((char *)key);
-        break;
+	hv = hash_cstr((char *)key);
+	break;
     case HATRACK_DICT_KEY_TYPE_OBJ_PTR:
-        hv = hash_pointer(key);
-        break;
+	hv = hash_pointer(key);
+	break;
     default:
-        abort();
+	abort();
     }
 
     if (offset != (int32_t)HATRACK_DICT_NO_CACHE) {
-        *(hatrack_hash_t *)(((uint8_t *)key) + offset) = hv;
+	*(hatrack_hash_t *)(((uint8_t *)key) + offset) = hv;
     }
 
     return hv;
@@ -329,6 +469,7 @@ hatrack_dict_record_cleanup(void *void_record)
 {
     hatrack_dict_t      *dict;
     hatrack_dict_item_t *record;
+    
 
     record = (hatrack_dict_item_t *)void_record;
     dict   = (hatrack_dict_t *)record->associated_dict;
